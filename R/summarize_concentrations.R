c8dd26ae325593e445d0e71f9477724607faa0ea
Christoph Leberecht <leberech@hs-mittweida.de>
2020-01-06 13:16:58 +0100

    summarize_concentrations now also supports collectors pointing to the same entities

--- R/summarize_concentrations.R
+++ R/summarize_concentrations.R
@@ -5,18 +5,32 @@
 #' @import dplyr
 #' @export
 summarize_concentrations <- function(trajectory, entity_mapping) {
-  # create hash environment
-  collector_hash <- new.env(hash = TRUE)
-  # add entity mapping
-  for (entry_index in 1:nrow(entity_mapping)) {
-    entry <- entity_mapping[entry_index,]
-    collector_hash[[entry$entity]] <- entry$collector
+  # retrieve collector names from mapping
+  collector_names <- entity_mapping %>%
+    distinct(collector) %>%
+    pull(collector)
+  # list to cellect rows
+  collecting_list = list()
+  # for each collector
+  for (filter_name in collector_names) {
+    # get mapping for entity
+    entities <- entity_mapping %>%
+      filter(collector == filter_name) %>%
+      pull(entity)
+    # add to list
+    collecting_list[[filter_name]] <- trajectory %>%
+      # look for entity in mapping
+      filter(entity %in% entities) %>%
+      # group without entity since it was filtered
+      group_by(time, node, section) %>%
+      # sum equivalent entitiies
+      summarise(concentration = sum(concentration)) %>%
+      ungroup() %>%
+      # add entity column
+      mutate(entity = filter_name)
   }
-  # apply entity transformations
-  trajectory %>%
-    # replace entitys with mapped collectors from hash table
-    mutate(entity = unlist(mget(entity, envir = collector_hash))) %>%
-    group_by(time, node, section, entity) %>%
-    # sum equivalent entitiies
-    summarise(concentration = sum(concentration))
+  # cosmetics and return
+  bind_rows(collecting_list) %>%
+    arrange(time) %>%
+    select(time, node, section, entity, concentration)
 }
